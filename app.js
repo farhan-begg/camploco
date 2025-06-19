d93544a0ddf2d6b7ac3a20016b44a99152f20e85
Farhan Begg <farhan.csbegg@gmail.com>
2025-06-18 22:08:03 -0700

    implemented error handling for asyn functions routes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
   .then(() => {
@@ -39,35 +42,40 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync( async (req, res, next) => {
   const campground = new Campground(req.body.campground)
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
-});
+}));
 
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit',catchAsync( async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id',catchAsync( async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync( async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
-})
+}))
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
